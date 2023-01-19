c64c2b36a5c89d0c31f8a4680d0140ec4c7570cf
Thiago Thamiel <thiago@thamiel.com>
2023-01-19 11:09:09 -0300

    Add files via upload

---
+++ OpenAI-ChatGPT/Program.cs
@@ -0,0 +1,10 @@
+﻿using OpenAI;
+
+OpenAIAPI api = new OpenAIAPI("YOUR-TOKEN",Engine.Davinci);
+
+var results = 
+    api.Completions.CreateCompletionsAsync(new CompletionRequest("Quando o Brasil foi descoberto?",
+        temperature: 0.1, max_tokens: 2000), 1).Result;
+
+Console.WriteLine(results.Completions[0].Text);
+Console.ReadKey();
